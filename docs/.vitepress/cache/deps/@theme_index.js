3b3f0b1660d6038393d054530f96356149cabcbf
xiaoyu.bi <xiaoyu.bi@lepudigital.com>
2023-03-08 00:28:54 +0800

    fix: 配置文件

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -1,24 +1,24 @@
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import NotFound from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
-import { default as default2 } from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default3 } from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default4 } from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default5 } from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default6 } from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default7 } from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default8 } from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default9 } from "/Users/bixiaoyu/2023/vitepress-starter/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import NotFound from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
+import { default as default2 } from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "/Users/bixiaoyu/2023/web-learn-note/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 var theme = {
   Layout,
   NotFound,
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,15 @@
 export default {
   title: 'VitePress',
-  description: 'Just playing around.'
+  description: 'Just playing around.',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/0/0.1' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      }
+    ]
+  }
 }
